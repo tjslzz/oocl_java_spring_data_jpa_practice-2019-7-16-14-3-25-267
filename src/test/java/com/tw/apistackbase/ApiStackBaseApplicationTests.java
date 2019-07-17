83c9f8c5b7b5e584d10a5087eb6dd5bdedbe3bb2
JERRY LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIJE9@OOCL.COM>
2019-07-17 16:46:00 +0800

    [$jarryLi$] test: should be able to create and fetch entity

--- src/test/java/com/tw/apistackbase/ApiStackBaseApplicationTests.java
+++ src/test/java/com/tw/apistackbase/ApiStackBaseApplicationTests.java
@@ -1,12 +1,17 @@
 package com.tw.apistackbase;
 
 import com.tw.apistackbase.core.Company;
+import com.tw.apistackbase.core.SingleEntity;
+import com.tw.apistackbase.core.SingleEntityRepository;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -21,49 +26,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-@ExtendWith(SpringExtension.class)
+import java.util.List;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@DataJpaTest
 public class ApiStackBaseApplicationTests {
 
 	@Autowired
-	private MockMvc mockMvc;
+	private SingleEntityRepository singleEntityRepository;
 
 	@Test
-	public void should_return_true_company_when_post_company() throws Exception {
-		mockMvc.perform(post("/companies").contentType("application/json;charset=UTF-8").content("{\"name\":\"felicity\"}"))
-				.andDo(print())
-				.andExpect(status().isOk())
-				.andExpect(content().json("{\n" +
-						"    \"id\": 35,\n" +
-						"    \"name\": \"felicity\"\n" +
-						"}"));
+	@DirtiesContext
+	public void should_be_able_to_create_and_fetch_entity(){
+		SingleEntity singleEntity = new SingleEntity("jerryLi");
+		singleEntityRepository.save(singleEntity);
+		List<SingleEntity> result = (List<SingleEntity>) singleEntityRepository.findAll();
+		assertEquals(1,result.size());
 	}
-
-	@Test
-	public void should_return_company_list_when_get_companies() throws Exception {
-		mockMvc.perform(get("/companies"))
-				.andDo(print())
-				.andExpect(status().isOk())
-				.andExpect(content().json("[\n" +
-						"    {\n" +
-						"        \"id\": 1,\n" +
-						"        \"name\": \"jerryLi\"\n" +
-						"    },\n" +
-						"    {\n" +
-						"        \"id\": 33,\n" +
-						"        \"name\": \"felicity\"\n" +
-						"    },\n" +
-						"    {\n" +
-						"        \"id\": 34,\n" +
-						"        \"name\": \"felicity\"\n" +
-						"    },\n" +
-						"    {\n" +
-						"        \"id\": 35,\n" +
-						"        \"name\": \"felicity\"\n" +
-						"    }\n" +
-						"]"));
-	}
-
 }
